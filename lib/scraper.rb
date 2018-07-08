14254e8d5777c9cd08f7de246e59acfe6bb0fef9
Vishal Sharma <vsh1819@gmail.com>
2018-07-08 18:22:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,7 @@ class Scraper
     
     doc.css("div.social-icon-container").each_with_index do |card, index|
       card.css("a").each_with_index do |student, index|
-        if index == 0
+        if index == 0 && student != ni
         result[:twitter] = student.attribute("href").value 
         end
        if index == 1
