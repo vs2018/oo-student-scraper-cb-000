1007ea7e04b361fce9e08a4ed915dceec2b2345c
Vishal Sharma <vsh1819@gmail.com>
2018-07-08 18:42:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,9 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |student|
+      student.each do |key, value| 
+      self.send("#{key}=", value)
+    end
     end
     
     
